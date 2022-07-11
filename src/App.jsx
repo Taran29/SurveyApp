d2c4bea7c22e4532797aed36c84d69f529ede9ec
Taranpreet <taranpreetsingh29@gmail.com>
2022-07-11 14:09:35 +0530

    Added Pagination and Dynamic Links to Home page

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,12 @@ const App = () => {
         <Router>
           <Navbar existingUser={existingUser} setExistingUser={setExistingUser} />
           <Routes>
-            <Route path="*" element={<Navigate to="/home" />} />
+            <Route path="*" element={<Navigate to={`/home/page/${1}`} />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login setExistingUser={setExistingUser} />} />
             <Route path="/security" element={<Security setExistingUser={setExistingUser} />} />
             <Route path="/forgotPassword" element={<ForgotPassword />} />
-            <Route path="/home" element={<Home setExistingUser={setExistingUser} />} />
+            <Route path="/home/page/:page" element={<Home setExistingUser={setExistingUser} />} />
             <Route path="/createSurvey" element={<CreateSurvey />} />
             <Route path="/addQuestions" element={<AddQuestions />} />
             <Route path="/surveyCreated" element={<SurveyCreated />} />
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,30 @@
 import { useState, useEffect } from 'react'
 import './Home.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Home = () => {
 
   const [surveys, setSurveys] = useState([])
+  const [pageNumber, setPageNumber] = useState()
+
+  const [isConnection, setIsConnection] = useState(true)
+  const [totalPages, setTotalPages] = useState('')
+
+  const [aboveTotal, setAboveTotal] = useState(false)
 
   const navigate = useNavigate()
+  let { page } = useParams()
 
   useEffect(() => {
+    console.log(page)
+    setAboveTotal(false)
+    if (isNaN(parseInt(page)) || parseInt(page) < 1) {
+      setPageNumber(1)
+      navigate(`/home/page/${1}`)
+    } else {
+      setPageNumber(parseInt(page))
+    }
+
     const getSurveys = async () => {
       let fetchObj = {
         method: 'GET'
@@ -22,37 +38,76 @@ const Home = () => {
         }
       }
 
-      const response = await fetch(`${process.env.REACT_APP_BASE_URL_API}/api/survey/`, fetchObj)
+      try {
+        const response = await fetch(`${process.env.REACT_APP_BASE_URL_API}/api/survey/page/${pageNumber}`, fetchObj)
+        const result = await response.json()
+        setSurveys(result.body.surveys)
+        setTotalPages(result.body.totalPages)
+        if (parseInt(page) > result.body.totalPages) {
+          setAboveTotal(true)
+        }
+      } catch (ex) {
+        setIsConnection(false)
+      }
 
-      const result = await response.json()
-      setSurveys(result.body)
     }
     getSurveys()
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [pageNumber]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="home-container">
-      <h2 className='home-container-title'>Available Surveys</h2>
-      {surveys.map((survey, idx) => {
-        return (
-          <div key={idx} className='home-survey'>
-            <div className='home-survey-meta'>
-              <span className='home-survey-title'> {survey.title} </span>
-              <span className='home-survey-category'> {survey.category} </span>
-            </div>
+      {isConnection && !aboveTotal &&
+        <>
+          <h2 className='home-container-title'>Available Surveys</h2>
+          {surveys.map((survey, idx) => {
+            return (
+              <div key={idx} className='home-survey'>
+                <div className='home-survey-meta'>
+                  <span className='home-survey-title'> {survey.title} </span>
+                  <span className='home-survey-category'> {survey.category} </span>
+                </div>
+                <button
+                  type="button"
+                  className='submitBtn fill-survey-btn'
+                  onClick={() => console.log(idx)}
+                >Fill</button>
+              </div>
+            )
+          })}
+
+          <div className="page-controls">
             <button
-              type="button"
-              className='submitBtn fill-survey-btn'
-              onClick={() => console.log(idx)}
-            >Fill</button>
+              className={pageNumber === 1 ? 'page-control-arrow-inactive' : 'page-control-arrow'}
+              onClick={() => {
+                setPageNumber(prev => prev - 1)
+                navigate(`/home/page/${pageNumber - 1}`)
+              }}
+            >🡐</button>
+            <div className="page-number">Page {pageNumber} of {totalPages}</div>
+            <button
+              className={pageNumber === totalPages ? 'page-control-arrow-inactive' : 'page-control-arrow'}
+              onClick={() => {
+                setPageNumber(prev => prev + 1)
+                navigate(`/home/page/${pageNumber + 1}`)
+              }}
+            >🡒</button>
           </div>
-        )
-      })}
-      <button
-        type="button"
-        className='submitBtn create-btn'
-        onClick={() => navigate('/createSurvey')}
-      >➕</button>
+
+          <button
+            type="button"
+            className='submitBtn create-btn'
+            onClick={() => navigate('/createSurvey')}
+          >➕</button>
+        </>
+      }
+
+      {!isConnection &&
+        <div>Cannot connect to the server right now. Please check your internet connection and try again later.</div>
+      }
+
+      {aboveTotal &&
+        <div>Invalid request. URL exceeds maximum page number.</div>
+      }
     </div>
   )
 }
